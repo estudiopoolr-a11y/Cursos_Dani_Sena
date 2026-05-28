ce6e52b16c89dcf8e6288a492ac453e84dbb826e
Pool <estudiopoolr@gmail.com>
2026-05-28 17:02:38 -0500

    Tu mensaje del commit

---
+++ Java_SCript/ex01/java.js
@@ -0,0 +1,2 @@
+document.write("Hola Mundo"); //Esto se ve el DOM, es decir, en el navegador
+console.log("Hola Mundo");//Esto se ve en la consola del navegador, es decir, en el inspector
---
+++ Java_SCript/ex02/java.js
@@ -0,0 +1,29 @@
+var nombre;//Declaramos la variable nombre
+nombre = "Dani";//Asignamos el valor "Dani" a la variable nombre
+console.log(nombre);//Mostramos el valor de la variable nombre en la consola del navegador
+
+var nombre = "Maria";// Reasignacion de un nuevo valor a la variable nombre
+console.log(nombre);//Mostramos el nuevo valor de la variable nombre en la consola del navegador
+
+var nombre = "Dani";//Reasignacion de un nuevo valor a la variable nombre
+console.log(nombre);//Mostramos el nuevo valor de la variable nombre en la consola del navegador
+
+
+/*
+    Uso del la palabra reservada 'let' para declarar una variable llamada 'bebida' y asignarle diferentes valores a lo largo del código. Cada vez que se asigna un nuevo valor a la variable 'bebida', se muestra su contenido en la consola.
+
+    Diferencia entre var y let: La palabra reservada 'var' tiene un alcance de función, lo que significa que la variable declarada con 'var' es accesible dentro de la función en la que se declara. Por otro lado, 'let' tiene un alcance de bloque, lo que significa que la variable declarada con 'let' solo es accesible dentro del bloque en el que se declara (por ejemplo, dentro de un bucle o una estructura condicional). Además, 'let' no permite redeclarar la misma variable dentro del mismo bloque, mientras que 'var' sí lo permite.
+*/
+
+let bebida;//Declaramos la variable bebida
+bebida = "Coca-cola";//Asignamos el valor "Coca-cola" a la variable bebida
+console.log(bebida);//Mostramos el valor de la variable bebida en la consola del navegador
+
+bebida = "Pepsi";//Reasignamos un nuevo valor a la variable bebida
+console.log(bebida);//Mostramos el nuevo valor de la variable bebida en la consola del navegador
+bebida = "Fanta";//Reasignamos un nuevo valor a la variable bebida
+console.log(bebida);//Mostramos el nuevo valor de la variable bebida en la consola del navegador
+
+/* Uso de 'const' para declarar una variable llamada 'precio' y asignarle un valor que no cambiará a lo largo del código conts PI */
+const PI = 3.1416;//Declaramos la constante PI y le asignamos el valor de 3.1416
+console.log(PI);//Mostramos el valor de la constante PI en la consola del navegador
